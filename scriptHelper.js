bb3cd0eb2a2e70fb08cc987eb3746480cb7de447
Ndietz21 <100815057+Ndietz21@users.noreply.github.com>
2022-12-07 21:45:40 -0600

    Add files via upload

--- scriptHelper.js
+++ scriptHelper.js
@@ -58,22 +58,22 @@ console.log(cargoLevel);
         fuelStatus.innerHTML = "Fuel level too low for launch";
         cargoStatus.innerHTML = "Cargo mass low enough for launch"
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-        launchStatus.style.color = "red";
+        launchStatus.style.color = 'rgb(199, 37, 78)';
     } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
         fuelStatus.innerHTML = "Fuel level high enough for launch"
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-        launchStatus.style.color = "#red";
+        launchStatus.style.color = 'rgb(199, 37, 78)';
     } else if (fuelLevel < 10000 && cargoLevel > 10000) {
         fuelStatus.innerHTML = "Fuel level too low for launch";
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-        launchStatus.style.color = "red";
+        launchStatus.style.color = 'rgb(199, 37, 78)';
     } else {
         fuelStatus.innerHTML = "Fuel level high enough for launch"
         cargoStatus.innerHTML = "Cargo mass low enough for launch"
         launchStatus.innerHTML = "Shuttle is Ready for Launch";
-        launchStatus.style.color = "green";
+        launchStatus.style.color = 'rgb(65, 159, 106)';
     }
 }
 
